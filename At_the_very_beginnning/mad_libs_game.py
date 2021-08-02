29b073c91f881281ec545ea6131714957db4496e
ChanglingXianjun <71072050+ChanlingXianjun@users.noreply.github.com>
2021-08-03 00:19:43 +0800

    Add files via upload

---
+++ At_the_very_beginnning/mad_libs_game.py
@@ -0,0 +1,9 @@
+# In this game basiclly I am going to let you input a bruch of
+# words and put them into my text, that makes a funny story.
+color = input('Enter a color you love ')
+plural_noun = input('Enter a plural noun ')
+celebrity = input('Enter a celebrity ')
+
+print('Roses are ' + color)
+print(plural_noun + ' are blue')
+print('I love ' + celebrity)
