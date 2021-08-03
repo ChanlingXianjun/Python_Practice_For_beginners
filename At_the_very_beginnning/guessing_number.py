281b590750d6842d03d133bc58dda607d6030d1b
ChanglingXianjun <71072050+ChanlingXianjun@users.noreply.github.com>
2021-08-03 11:59:23 +0800

    Add files via upload

---
+++ At_the_very_beginnning/guessing_number.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+guess the number and have fun :)
+"""
+import random
+
+number = random.randrange(1, 100)
+running = True
+
+while running:
+    guess = int(input("Guess a number(1~100):"))
+
+    if guess == number:
+        print ("Congratulations! You win!")
+        running = False
+
+    elif guess < number:
+        print ("Too small! :(")
+
+    else:
+        print ("Too big! :(")
