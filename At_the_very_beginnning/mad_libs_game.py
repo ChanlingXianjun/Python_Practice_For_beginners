4b37a21b6be8f1c5d144f00cb0935cb76cf0431d
ChanglingXianjun <71072050+ChanlingXianjun@users.noreply.github.com>
2021-08-05 22:02:43 +0800

    Add files via upload

--- At_the_very_beginnning/mad_libs_game.py
+++ At_the_very_beginnning/mad_libs_game.py
@@ -1,5 +1,11 @@
 # In this game basiclly I am going to let you input a bruch of
 # words and put them into my text, that makes a funny story.
+# A few ways to do this:
+# youtuber = 'Chanling'
+# print('subscribe to ' + youtuber)
+# print('subscribe to {}'.format(youtuber))
+# print(f'subscribe to {youtuber}')
+
 color = input('Enter a color you love ')
 plural_noun = input('Enter a plural noun ')
 celebrity = input('Enter a celebrity ')
